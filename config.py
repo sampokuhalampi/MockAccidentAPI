7e2162a170996326979920731ac91649b944e46b
sampokuhalampi <sampo.kuhalampi@icloud.com>
2024-04-04 23:28:24 +0300

    SCRUM-150 API team: Delete endpoint for MockAccidentAPI

--- config.py
+++ config.py
@@ -5,5 +5,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'ASD-database.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # Keep your API_KEY here (ensure it matches the one used in API calls)
     API_KEY = os.environ['API_KEY']
